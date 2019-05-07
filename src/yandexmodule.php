ea6c361bc41ac52927b86f44ac883d39b0029676
Антон Н. Николаев <nikolaevanym@yamoney.ru>
2019-05-07 09:43:51 +0300

    Исправлена ошибка генерации прайса в Маркет

--- src/yandexmodule.php
+++ src/yandexmodule.php
@@ -89,7 +89,7 @@ class YandexModule extends PaymentModule
 
         $this->name            = 'yandexmodule';
         $this->tab             = 'payments_gateways';
-        $this->version         = '1.1.6';
+        $this->version         = '1.1.7';
         $this->author          = $this->l('Yandex.Money');
         $this->need_instance   = 1;
         $this->bootstrap       = 1;
